6775cbffaaada7d4cac109952ab8e51c7be5cddf
razidal <razi.dallasheh@gmail.com>
2024-09-04 18:26:02 +0300

    sv

--- front/src/pages/home/CustomCarousel.jsx
+++ front/src/pages/home/CustomCarousel.jsx
@@ -104,7 +104,7 @@ const CustomCarousel = () => {
           <img
             src={images[index].src}
             alt={`slide-${index}`}
-            style={{ height: "296px", width: "313.53px", objectFit: 'cover', marginBottom:'100px'}}
+            style={{ height: "296px", width: "313.53px", objectFit: 'cover', paddingBottom:'50px'}}
           />
           <Typography variant="h3" className="slider-title">
             {images[index].title}
