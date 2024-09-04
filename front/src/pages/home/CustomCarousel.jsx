1309d245eca7a54a7fbe59e6c23335da1d080a10
razidal <razi.dallasheh@gmail.com>
2024-09-04 14:34:37 +0300

    sv

--- front/src/pages/home/CustomCarousel.jsx
+++ front/src/pages/home/CustomCarousel.jsx
@@ -1,51 +1,48 @@
-import React, { useState } from 'react';
-import { Box, Button, Grid } from '@mui/material';
-import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import wedding from "./wedding.png";
+import slider2 from "./slider2.png";
+import slider3 from "./slider3.png";
+import slider4 from "./slider4.png";
+
 
 const images = [
-  './wedding.png',
-  './slider2.png',
-  './slider3.png',
-'./slider4.png',
+  { src: wedding, caption: "Special Offer: Wedding Discount" },
+  { src: slider2, caption: "Special Offer: Up to 50% Off" },
+  { src: slider3, caption: "Exclusive: Limited Time Offer" },
+  { src: slider4, caption: "Flash Sale: Don't Miss Out" }
 ];
 
 const CustomCarousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handleBack = () => {
+  const handlePrev = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
 
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
-    <Box sx={{ width: '100%', overflow: 'hidden' }}>
-      <Grid container justifyContent="center" alignItems="center">
-        <Grid item>
-          <Button onClick={handleBack}>
-            <KeyboardArrowLeft />
-          </Button>
-        </Grid>
-        <Grid item>
-          <Box
-            component="img"
-            src={images[currentIndex]}
-            alt="Carousel Slide"
-            sx={{ height: 400, width: '100%', objectFit: 'cover' }}
-          />
-        </Grid>
-        <Grid item>
-          <Button onClick={handleNext}>
-            <KeyboardArrowRight />
-          </Button>
-        </Grid>
-      </Grid>
+    <Box sx={{ position: "relative", maxWidth: "100%", textAlign: "center" }}>
+      <Box component="img" 
+           src={images[currentIndex].src} 
+           alt={images[currentIndex].caption}
+           sx={{ width: "100%", height: "auto" }} 
+      />
+      <Typography variant="h6" sx={{ position: "absolute", bottom: 10, left: 0, right: 0, color: "#fff", backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+        {images[currentIndex].caption}
+      </Typography>
+      <Button onClick={handlePrev} sx={{ position: "absolute", top: "50%", left: 0, transform: "translateY(-50%)" }}>Prev</Button>
+      <Button onClick={handleNext} sx={{ position: "absolute", top: "50%", right: 0, transform: "translateY(-50%)" }}>Next</Button>
     </Box>
   );
 };
+
 
 export default CustomCarousel;
