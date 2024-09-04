f4915555bbf6be96b5d9de1406341d0809d6406a
razidal <razi.dallasheh@gmail.com>
2024-09-04 18:20:46 +0300

    sv

--- front/src/components/footer/footer.jsx
+++ front/src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import { Box, Typography, Link } from '@mui/material';
 export default function Footer() {
   return (
     <Box sx={{ bgcolor: '#f8f8f8', p: 2, mt: 4, textAlign: 'center' }}>
-      <Typography variant="body2" color="textSecondary">
+      <Typography variant="body2" color="textSecondary" gutterBottom>
         © {new Date().getFullYear()} - Event Decor. All rights reserved.
       </Typography>
       <Typography variant="body2" color="textSecondary">
