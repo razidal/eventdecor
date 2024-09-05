a233b5042bcb449a9c2f483a73321ac4ef7bf0d9
razidal <razi.dallasheh@gmail.com>
2024-09-05 21:41:32 +0300

    sv

--- front/src/pages/admin/management/TableAdmin.jsx
+++ front/src/pages/admin/management/TableAdmin.jsx
@@ -1,58 +1,144 @@
 import React, { useState, useEffect } from "react";
-import { Container, Box, Typography, Grid } from "@mui/material";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+  Modal,
+  Box,
+  Container,
+} from "@mui/material";
 import axios from "axios";
-import OrdersCard from "./OrdersCard";
+import Typography from '@mui/material/Typography';
 
 const TableAdmin = () => {
-  const [orders, setOrders] = useState([]);
+  const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null); // State to store the ID of the selected user
 
-  const fetchOrders = async () => {
-    try {
-      const response = await axios.get(
-        `https://backstore-iqcq.onrender.com/cart/allOrders`,
-        {
-          timeout: 5000,
-        }
-      );
-      setOrders(response.data.orders); // Adjust based on actual response structure
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-      setLoading(false);
-    }
+  const handleOpenModal = (user) => {
+    setSelectedUser(user);
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
   };
 
   useEffect(() => {
-    fetchOrders();
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://backstore-iqcq.onrender.com/cart/allOrders`,
+          {
+            timeout: 5000,
+          }
+        );
+
+        setUserData(response.data.orders);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <Container>
-      <Box sx={{ textAlign: 'center' }}>
-        <Typography variant="h4" sx={{ paddingTop: '50px', paddingBottom: '30px', display: 'inline-block' }}>
-          Orders
-        </Typography>
-      </Box>
+    <div>
+       <Box sx={{ textAlign: 'center' }}>
+          <Typography variant="h4" sx={{ paddingTop: '50px', paddingBottom:'30px', display: 'inline-block' }}>
+            Orders
+          </Typography>
+        </Box>
       {loading ? (
         <Box sx={{ textAlign: 'center' }}>
-          <Typography variant="h6" sx={{ display: 'inline-block' }}>
-            Loading...
-          </Typography>
+        <Typography variant="h6" sx={{ display: 'inline-block' }}>
+          Loading...
+        </Typography>
         </Box>
       ) : error ? (
         <p>Error: {error.message}</p>
       ) : (
-        <Grid container spacing={2}>
-          {orders.map((order) => (
-            <Grid item xs={12} sm={6} md={4} key={order._id}>
-              <OrdersCard order={order} fetchOrders={fetchOrders} />
-            </Grid>
-          ))}
-        </Grid>
+        <Container>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Order Number</TableCell>
+                <TableCell>Total Price</TableCell>
+                <TableCell>Customer Name</TableCell>
+                <TableCell>Order Details</TableCell>
+                <TableCell>Order Confirmation</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {userData?.map((user) => (
+                <TableRow key={user._id}>
+                  <TableCell>{user._id}</TableCell>
+                  <TableCell>{user.totalAmount}</TableCell>
+                  <TableCell>{user.userId?.fullName}</TableCell>
+                  <TableCell>
+                    <Button onClick={() => handleOpenModal(user)}>
+                      Order Details
+                    </Button>
+                  </TableCell>
+                  <TableCell>Confirmed</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Container>
       )}
-    </Container>
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <div>
+          <Box
+            component="form"
+            sx={{
+              "& > :not(style)": { m: 1, width: "25ch" },
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor: "white",
+              justifyContent: "center",
+              gap: "1rem",
+              width: "50%",
+              margin: "auto",
+            }}
+          >
+            <h2>Order Details</h2>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Product Name</TableCell>
+                  <TableCell>Price</TableCell>
+                  <TableCell>Quantity</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {selectedUser?.products?.map((product, productIndex) => (
+                  <TableRow key={productIndex}>
+                    <TableCell>{product.productId?.name}</TableCell>
+                    <TableCell>{product.price}$</TableCell>
+                    <TableCell>{product.quantity}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <h3>Delivery Address</h3>
+            <p>
+              {selectedUser?.address?.street}, {selectedUser?.address?.city},{" "}
+              {selectedUser?.address?.postalCode}, {selectedUser?.address?.country}
+            </p>
+          </Box>
+        </div>
+      </Modal>
+    </div>
   );
 };
 
