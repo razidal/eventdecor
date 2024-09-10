bbb70659519ef6ef17d6c243fa8576c938335263
razidal <razi.dallasheh@gmail.com>
2024-09-10 20:52:18 +0300

    sv

--- front/src/components/footer/footer.jsx
+++ front/src/components/footer/footer.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Box, Typography, Link } from '@mui/material';
+import { Box, Typography, Link ,Container} from '@mui/material';
 
 export default function Footer() {
   return ( 
-    <Box sx={{ bgcolor: '#f8f8f8', p: 2, mt: 4, textAlign: 'center' }}> {/* Added mt: 4 for margin-top */}
+    <Box sx={{
+      minHeight: '100vh', // Full viewport height
+      display: 'flex',
+      flexDirection: 'column',
+    }}> {/* Added flex container styles */}   
+    <Container component="main" sx={{ flexGrow: 1, py: 3 }}> {/* Added flexGrow and py for vertical centering */}   
       <Typography variant="body2" color="textSecondary" gutterBottom> {/* Added gutterBottom for bottom margin */}
         © {new Date().getFullYear()} - Event Decor. All rights reserved. {/* Updated the year dynamically */}
       </Typography> 
       <Typography variant="body2" color="textSecondary">
         Contact: <Link href="mailto:eventdeocr@gmail.com">eventdeocr@gmail.com</Link> {/* Added contact information */}   
       </Typography>
+      </Container>
     </Box>
   );
 }
