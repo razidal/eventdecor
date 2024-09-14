80788fa6c5df93823d9a896f2898f2c3359c9dab
razidal <razi.dallasheh@gmail.com>
2024-09-14 17:37:52 +0300

    sv

--- front/src/pages/signin/SignIn.jsx
+++ front/src/pages/signin/SignIn.jsx
@@ -137,7 +137,6 @@ const SignIn = () => {
           setNewPassword("");
           setConfirmPassword("");
           navigate("/SignIn"); // Redirect to the sign-in page after successful password reset
-          window.location.reload();
         } catch (error) {
           setError("Failed to reset password. Please try again.");
         }
