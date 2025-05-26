40893cba310ed782f2d2e5f074b2b8d1ba0f7e9b
razidal <razi.dallasheh@gmail.com>
2025-05-26 17:41:13 +0300

    sv

--- back/db.js
+++ back/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => { // async function to handle promise
   try {
     mongoose // mongoose library
-      .connect("mongodb+srv://razidal:eventdecor123@cluster0.eldvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") // connect to MongoDB database
+      .connect("mongodb+srv://razidal:eventdecor23@cluster0.eldvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") // connect to MongoDB database
       .then(() => console.log("Connected to MongoDB")) // log success message
       .catch((err) => console.error("Could not connect to MongoDB", err)); // log error message
     console.log("MongoDB connection SUCCESS");
