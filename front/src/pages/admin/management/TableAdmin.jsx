b3e289d9805663cd890185f03b4e02da113b5929
razidal <razi.dallasheh@gmail.com>
2025-05-29 00:33:12 +0300

    sv

--- front/src/pages/admin/management/TableAdmin.jsx
+++ front/src/pages/admin/management/TableAdmin.jsx
@@ -19,6 +19,9 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import axios from "axios";
 import Typography from '@mui/material/Typography';
+import {useSelector } from "react-redux";
+import Cookies from "js-cookie";
+import React from "react";
 
 const TableAdmin = () => {
   const [userData, setUserData] = useState(null);
@@ -26,7 +29,7 @@ const TableAdmin = () => {
   const [error, setError] = useState(null);
   const [openModal, setOpenModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null); // State to store the ID of the selected user
-
+  const user = useSelector((state) => state.user.user);
   const isMobile = useMediaQuery("(max-width:600px)"); // for mobile devices
 
   const handleOpenModal = (user) => {
@@ -38,6 +41,8 @@ const TableAdmin = () => {
     setOpenModal(false);
   };
 
+  const userCookies = Cookies.get("user"); // Get user data from cookies
+  const userfullName = userCookies ? JSON.parse(userCookies).fullName : null;
   const formatOrderDate = (isoString) => {
   if (!isoString) return "";
   const date = new Date(isoString);
@@ -132,7 +137,7 @@ const TableAdmin = () => {
                   <TableCell align="center">{user._id}</TableCell>
                   <TableCell align="center">{user.totalAmount}$</TableCell>
                   <TableCell align="center">{formatOrderDate(user.orderDate)}</TableCell>
-                  <TableCell align="center">{user.userId?.fullName}</TableCell>
+                  <TableCell align="center">{userfullName}</TableCell>
                   <TableCell>
                     <Button align="center" onClick={() => handleOpenModal(user)}>
                       Order Details
